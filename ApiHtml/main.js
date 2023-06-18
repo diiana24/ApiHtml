bdddc5a0e0a0a2b5643d64aa89f427ecf85dc197
diiana24 <93978982+diiana24@users.noreply.github.com>
2023-06-18 21:55:02 +0200

    Add files via upload

---
+++ ApiHtml/main.js
@@ -0,0 +1,72 @@
+const fileSelector = document.querySelector("input[type=file]");
+const video = document.querySelector("video");
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+const volUp = document.getElementById("vol-up");
+const volDown = document.getElementById("vol-down");
+const loadingMessage = document.getElementById('loading-message');
+const videoPlayer = document.getElementById('video-player');
+
+function isFileApiSupported() {
+  return 'File' in window && 'FileReader' in window && 'Blob' in window;
+}
+
+function isValidVideoFile(file) {
+  const acceptedMimeTypes = [
+    'video/mp4',
+    'video/webm',
+    'video/ogg',
+  ];
+
+  return acceptedMimeTypes.includes(file.type);
+}
+
+if (!isFileApiSupported()) {
+  alert('Lo sentimos, tu navegador no es compatible con la File API. Por favor, actualiza a un navegador más moderno.');
+} else {
+  fileSelector.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+
+    if (file && isValidVideoFile(file)) {
+      loadingMessage.style.display = 'block';
+      const fileReader = new FileReader();
+
+      fileReader.onload = (event) => {
+        const blob = new Blob([event.target.result], { type: file.type });
+        const url = URL.createObjectURL(blob);
+        video.src = url;
+      };
+
+      fileReader.readAsArrayBuffer(file);
+
+    } else {
+      alert('Por favor, selecciona un archivo de vídeo válido.');
+      fileSelector.value = '';
+    }
+  });
+
+  play.addEventListener('click', () => {
+    video.play();
+  });
+
+  pause.addEventListener('click', () => {
+    video.pause();
+  });
+
+  volUp.addEventListener('click', () => {
+    if (video.volume < 1) {
+      video.volume += 0.1;
+    }
+  });
+
+  volDown.addEventListener('click', () => {
+    if (video.volume > 0) {
+      video.volume -= 0.1;
+    }
+  });
+
+  video.addEventListener('loadeddata', () => {
+    loadingMessage.style.display = 'none';
+    videoPlayer.style.display = 'block';
+  });
+}
